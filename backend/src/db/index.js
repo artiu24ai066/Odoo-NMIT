468b8b6ffefae8a3542d467aea46d9883dc6af1c
Arti Jangid <u24ai066@aid.svnit.ac.in>
2026-08-22 15:06:35 +0530

    database connected

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try{
         const databaseName = process.env.DB_NAME || "odoo";
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${databaseName}`)
+        const connectionUri = new URL(process.env.MONGODB_URI);
+        connectionUri.pathname = `/${databaseName}`;
+        const connectionInstance = await mongoose.connect(connectionUri.toString())
         console.log(`\n MongoDB connected! DB Host: ${connectionInstance.connection.host}`)
     } catch(error){
         console.log("MongoDB connection error", error);
